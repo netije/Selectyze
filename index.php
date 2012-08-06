33952d97e61c6382b52d20fe558b7ca6ebebe0fc
Mickaël SURREL <mickael@alpixel.fr>
2012-08-06 14:33:38 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,208 @@
+<?php
+require_once(dirname(__FILE__)."/../../../scripts/init.inc.php"); 
+$oMysql = MySQL::getInstance();
+$aNews = $oMysql->getNews(NB_NEWS);
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Selectyze Demonstration : jquery plugin | Make your own select lists with jQuery : MyjQueryPlugins.com</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="Language" content="en" />
+	<link rel="shortcut icon" href="<?php echo URL_SITE; ?>favicon.ico" />
+	<base href="<?php echo URL_SITE; ?>" />
+	<meta name="description" content="Selectyze is a jQuery plugin. Skin your HTML select lists easily with Selectyze. This plugin works with IE6-9, chrome, Safari. Selectyze hide your select element, and build 2 elements inside a container in order to simulate the behaviour of a select element. Very simple !" />
+	<meta name="robots" content="index, follow, all" />
+	<link rel="stylesheet" href="<?php echo URL_SITE; ?><?php echo URL_DEMO_SELECTYZE; ?>css/Selectyze.jquery.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo URL_SITE; ?>static/css/content.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo URL_SITE; ?>static/css/648.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo URL_SITE; ?>static/css/336.css" type="text/css" />
+	<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css' />
+</head>
+
+<body id="demopage">
+
+
+	<?php require_once(dirname(__FILE__)."/../../../includes/header.inc.php");?>
+
+<div id="main">
+
+	<div class="breadcrumb">
+		<a href="index" class="backTo">Home</a>
+		<a href="Selectyze" class="backTo">Selectyze Homepage</a>
+		<div class="clr"></div>
+	</div>
+	
+	<div id="left_col">
+
+		<h1>Selectyze demonstration</h1>
+		<?php
+		if(isset($_POST['btValide']))
+		{
+			echo '<div class="phpresponse">';
+			echo 'Datas received :<br />';
+			echo '#1 -> '.$_POST['style1'].'<br />';
+			echo '#2 -> '.$_POST['style2'].'<br />';
+			echo '#3 -> '.$_POST['style3'].'<br />';
+			echo '#4 -> '.$_POST['style4'].'<br />';
+			echo '#5 -> '.$_POST['style5'].'<br />';
+			echo '</div>';
+		}
+		?>
+		<form method="post" action="">
+			<fieldset>
+				
+				<div class="contain_select">
+					<p class="intro first">#1 -> $('.selectyze1').Selectyze({<br />theme : <span>'skype'</span>,<br />effectOpen : <span>'fade'</span><br />});</p>
+					<label>State</label>
+					<select name="style1" class="selectyze1">
+						<option>- -</option>
+						<optgroup label="States : optgroup detected !">
+							<option value="Alabama" <?php if(isset($_POST['style1']) && $_POST['style1'] == 'Alabama') echo 'selected="selected"';?>>Alabama</option>
+							<option value="Alaska" <?php if(isset($_POST['style1']) && $_POST['style1'] == 'Alaska') echo 'selected="selected"';?>>Alaska</option>
+							<option value="Arizona" <?php if(isset($_POST['style1']) && $_POST['style1'] == 'Arizona') echo 'selected="selected"';?>>Arizona</option>
+							<option value="Arkansas" <?php if(isset($_POST['style1']) && $_POST['style1'] == 'Arkansas') echo 'selected="selected"';?>>Arkansas</option>
+							<option value="California" <?php if(isset($_POST['style1']) && $_POST['style1'] == 'California') echo 'selected="selected"';?>>California</option>
+						</optgroup>
+						
+						<optgroup label="States : optgroup detected !">
+							<option value="Alabama" <?php if(isset($_POST['style1']) && $_POST['style1'] == 'Alabama') echo 'selected="selected"';?>>Alabama</option>
+							<option value="Alaska" <?php if(isset($_POST['style1']) && $_POST['style1'] == 'Alaska') echo 'selected="selected"';?>>Alaska</option>
+							<option value="Arizona" <?php if(isset($_POST['style1']) && $_POST['style1'] == 'Arizona') echo 'selected="selected"';?>>Arizona</option>
+							<option value="Arkansas" <?php if(isset($_POST['style1']) && $_POST['style1'] == 'Arkansas') echo 'selected="selected"';?>>Arkansas</option>
+							<option value="California" <?php if(isset($_POST['style1']) && $_POST['style1'] == 'California') echo 'selected="selected"';?>>California</option>
+						</optgroup>
+							
+					</select>
+					<div class="clr"></div>
+				</div>
+				
+				
+				
+					
+				<div class="contain_select">
+					<p class="intro">#2 -> $('.selectyze2').Selectyze({<br />theme : <span>'mac'</span>,<br />effectClose,<span>'fade'</span><br />});</p>
+					<label>State</label>
+					<select name="style2" class="selectyze2">
+						<option>- -</option>
+						<optgroup label="States">
+							<option value="Alabama" <?php if(isset($_POST['style2']) && $_POST['style2'] == 'Alabama') echo 'selected="selected"';?>>Alabama</option>
+							<option value="Alaska" <?php if(isset($_POST['style2']) && $_POST['style2'] == 'Alaska') echo 'selected="selected"';?>>Alaska</option>
+							<option value="Arizona" <?php if(isset($_POST['style2']) && $_POST['style2'] == 'Arizona') echo 'selected="selected"';?>>Arizona</option>
+							<option value="Arkansas" <?php if(isset($_POST['style2']) && $_POST['style2'] == 'Arkansas') echo 'selected="selected"';?>>Arkansas</option>
+							<option value="California" <?php if(isset($_POST['style2']) && $_POST['style2'] == 'California') echo 'selected="selected"';?>>California</option>
+						</optgroup>
+					</select>
+					<div class="clr"></div>
+				</div>
+				<div class="clr"></div>
+				
+				<div class="contain_select">
+					<p class="intro">#3 -> $('.selectyze3').Selectyze({<br />theme : <span>'grey'</span>,<br />effectOpen : <span>'fade'</span>,<br />effectClose : <span>'fade'</span><br />});</p>
+					<label>State</label>
+					<select name="style3" class="selectyze3">
+						<option>- -</option>
+						<optgroup label="States">
+							<option value="Alabama" <?php if(isset($_POST['style3']) && $_POST['style3'] == 'Alabama') echo 'selected="selected"';?>>Alabama</option>
+							<option value="Alaska" <?php if(isset($_POST['style3']) && $_POST['style3'] == 'Alaska') echo 'selected="selected"';?>>Alaska</option>
+							<option value="Arizona" <?php if(isset($_POST['style3']) && $_POST['style3'] == 'Arizona') echo 'selected="selected"';?>>Arizona</option>
+							<option value="Arkansas" <?php if(isset($_POST['style3']) && $_POST['style3'] == 'Arkansas') echo 'selected="selected"';?>>Arkansas</option>
+							<option value="California" <?php if(isset($_POST['style3']) && $_POST['style3'] == 'California') echo 'selected="selected"';?>>California</option>
+						</optgroup>
+					</select>
+					<div class="clr"></div>
+				</div>
+				
+				<div class="contain_select">
+					<p class="intro">#4 -> $('.selectyze4').Selectyze({ theme : <span>'firefox'</span>});</p>
+					<label>State</label>
+					<select name="style4" class="selectyze4">
+						<option>- -</option>
+						<optgroup label="States">
+							<option value="Alabama" <?php if(isset($_POST['style4']) && $_POST['style4'] == 'Alabama') echo 'selected="selected"';?>>Alabama</option>
+							<option value="Alaska" <?php if(isset($_POST['style4']) && $_POST['style4'] == 'Alaska') echo 'selected="selected"';?>>Alaska</option>
+							<option value="Arizona" <?php if(isset($_POST['style4']) && $_POST['style4'] == 'Arizona') echo 'selected="selected"';?>>Arizona</option>
+							<option value="Arkansas" <?php if(isset($_POST['style4']) && $_POST['style4'] == 'Arkansas') echo 'selected="selected"';?>>Arkansas</option>
+							<option value="California" <?php if(isset($_POST['style4']) && $_POST['style4'] == 'California') echo 'selected="selected"';?>>California</option>
+						</optgroup>
+					</select>
+					<div class="clr"></div>
+				</div>		
+				<div class="clr"></div>
+				
+				<div class="contain_select">
+					<p class="intro">#5 -> $('.selectyze5').Selectyze({ theme : <span>'css3'</span>});</p>
+					<label>State</label>
+					<select name="style5" class="selectyze5">
+						<option>- -</option>
+						<optgroup label="States">
+							<option value="Alabama" <?php if(isset($_POST['style5']) && $_POST['style5'] == 'Alabama') echo 'selected="selected"';?>>Alabama</option>
+							<option value="Alaska" <?php if(isset($_POST['style5']) && $_POST['style5'] == 'Alaska') echo 'selected="selected"';?>>Alaska</option>
+							<option value="Arizona" <?php if(isset($_POST['style5']) && $_POST['style5'] == 'Arizona') echo 'selected="selected"';?>>Arizona</option>
+							<option value="Arkansas" <?php if(isset($_POST['style5']) && $_POST['style5'] == 'Arkansas') echo 'selected="selected"';?>>Arkansas</option>
+							<option value="California" <?php if(isset($_POST['style5']) && $_POST['style5'] == 'California') echo 'selected="selected"';?>>California</option>
+						</optgroup>
+					</select>
+					<div class="clr"></div>
+				</div>
+				
+				<div class="contain_select">
+					Imagine all the theme you can make !<br />
+					No limit, only CSS &amp; images !
+				</div>
+				<div class="clr"></div>
+				<input type="submit" value="SUBMIT MY SELECT LISTS !" name="btValide" class="submit" />
+			</fieldset>
+		</form>
+
+		<!-- Advertise Bottom -->
+		<?php require_once(dirname(__FILE__)."/../../../includes/pub.inc.php");?>
+	</div>
+		
+	
+	<!-- RIGHT COLUMN -->
+	<?php require_once(dirname(__FILE__)."/../../../includes/col_right.inc.php");?>
+		
+	<div class="clr"></div>
+
+</div>
+	
+	
+<!-- FOOTER -->
+<?php require_once(dirname(__FILE__)."/../../../includes/footer.inc.php");?>
+
+	<script type="text/javascript" src="<?php echo URL_SITE; ?><?php echo URL_DEMO_SELECTYZE; ?>jquery/jquery.js"></script>
+	<script type="text/javascript" src="<?php echo URL_SITE; ?><?php echo URL_DEMO_SELECTYZE; ?>jquery/Selectyze.jquery.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('.selectyze1').Selectyze({
+				theme : 'skype',
+				effectOpen : 'fade'
+			});
+			
+			$('.selectyze2').Selectyze({
+				theme : 'mac',
+				effectClose:'fade'
+			});
+			
+			$('.selectyze3').Selectyze({
+				theme : 'grey',
+				effectOpen : 'fade',
+				effectClose : 'fade'
+			});	
+			
+			$('.selectyze4').Selectyze({
+				theme : 'firefox'
+			});
+			
+			$('.selectyze5').Selectyze({
+				theme : 'css3'
+			});
+			
+		});
+	</script>
+	
+	<?php require_once dirname(__FILE__)."/../../../includes/analytics.inc.php"; ?>
+</body>
+</html>
